6f6b346d3fedc8d15bd622af0918e9889d1d16a0
Fauzi Ferdiansyah <fetruzie2@gmail.com>
2019-12-04 15:31:31 +0700

    [add]:: page vacancy list per division

--- app/Http/Controllers/Customer/PagesController.php
+++ app/Http/Controllers/Customer/PagesController.php
@@ -6,29 +6,93 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Division;
 use App\JobVacancy;
+use App\JobVacancyDetail;
+use Carbon\Carbon;
 use DB;
 
 class PagesController extends Controller
 {
     public function index() {
-    	$data_job = Division::select([
-                'id','name'
-            ])
-    		->where('status', 1)
-            ->orderBy('id', 'DESC')->get();
-        $job_arr = [];
-           foreach ($data_job as $r) {
-           	$job_arr[] = (object) [
-           				'name' => $r->name,
-           				'available' => $this->getAvailableDiv($r->id)
-        			];
-           }
+    	$data_job = Division::select(['id','name'])
+                		->where('status', 1)
+                    ->orderBy('id', 'DESC')->get();
 
-        return view('customer.pages.index')
-            ->with(compact('job_arr'));
+      $job_arr = [];
+         foreach ($data_job as $r) {
+         	$job_arr[] = (object) [
+                'id' => $r->id,
+         				'name' => $r->name,
+         				'available' => $this->getAvailableDiv($r->id, 'count')
+      			];
+         }
+
+      return view('customer.pages.index')
+          ->with(compact('job_arr'));
     }
 
-    private function getAvailableDiv($get_id) {
-    	return $count_available = JobVacancy::where('division_id', $get_id)->count();
+    public function list($id) {
+      // Get Division
+      $data = Division::select(
+                  [
+                    'divisions.name'
+                  ]
+                )
+                ->where('divisions.id', $id)
+                ->first();
+      // Get All Vacancies
+      $vacancies = $this->getAvailableDiv($id, 'row');
+
+      return view('customer.pages.list')
+          ->with(compact('data','vacancies'));
+    }
+
+    private function getAvailableDiv($get_id, $type) {
+      $get_vacancy = JobVacancy::select(
+                        [
+                          'job_vacancies.id',
+                          'job_vacancies.title',
+                          'job_vacancies.end_date'
+                        ]
+                      )
+                      ->where('job_vacancies.division_id', $get_id)
+                      ->where('job_vacancies.display', 1)
+                      ->where('job_vacancies.start_date', '<=', Carbon::now())
+                      ->where('job_vacancies.end_date', '>=', Carbon::now())
+                      ->get();
+
+      $data_count = count($get_vacancy);
+      $data_arr = [];
+      if($data_count > 0) {
+        foreach ($get_vacancy as $key => $v) {
+            $data_arr[$key] = [
+                            'id' => $v->id,
+                            'title' => $v->title,
+                            'end_date' => $v->end_date
+                          ];
+            if($this->getAvailableDivCond($v->id, $get_id) == 0) {
+              unset($data_arr[$key]);
+            }
+        }
+      }
+      if($type == 'count'){
+        return count($data_arr);
+      }else if($type == 'row'){
+        return $data_arr;
+      }
+    	
+    }
+
+    private function getAvailableDivCond($id_vacation, $id_div) {
+      $get_vacancy_count = DB::table('job_vacancies')
+                            ->where('job_vacancies.division_id', $id_div)
+                            ->where('job_vacancies.id', $id_vacation)
+
+                            ->join('job_vacancy_details', 'job_vacancies.id' , '=', 'job_vacancy_details.job_vacancy_id')
+                            ->where('job_vacancies.display', 1)
+                            ->where('job_vacancy_details.status', 1)
+                            ->where('job_vacancies.start_date', '<=', Carbon::now())
+                            ->where('job_vacancies.end_date', '>=', Carbon::now())
+                            ->count();
+      return $get_vacancy_count;
     }
 }
---
+++ resources/views/customer/pages/list.blade.php
@@ -0,0 +1,46 @@
+@extends('customer.layouts.frontend')
+@section('title')List Vacancy @endsection
+
+@section('content')
+    <div class="page-header-area">
+        <div class="container">
+            <div class="head-text pgh">
+                <h3>
+                    <span>Vacancy Listing</span>
+                    <div class="division-name">{{ $data->name }}</div>
+                </h3>
+                <p class="sub-head"><a href="/" class="brd breadcrumb-a">Home</a><span class="brd breadcrumb-active">Vacancy Listing</span></p>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="block-vc-list">
+            <div class="row">
+                @foreach($vacancies as $r)
+                <div class="col-md-6">
+                    <div class="vc-list">
+                        <div class="vc-desc">
+                            <h4>{{ $r['title'] }}</h4>
+                            <span class="vc-time"><span class="text-dl">Deadline</span> {{ date('j F Y', strtotime($r['end_date'])) }}</span>
+                        </div>
+                        <div class="vc-apply">
+                            <a href="#" class="btn btn-block btn-apply">Apply Now</a>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+                {{-- <div class="col-md-6">
+                    <div class="vc-list">
+                        <div class="vc-desc">
+                            <h4>Web Designer</h4>
+                            <span class="vc-time"><span class="text-dl">Deadline</span> 18 September 2020</span>
+                        </div>
+                        <div class="vc-apply">
+                            <a href="#" class="btn btn-block btn-apply">Apply Now</a>
+                        </div>
+                    </div>
+                </div> --}}
+            </div>
+        </div>
+    </div>
+@endsection
