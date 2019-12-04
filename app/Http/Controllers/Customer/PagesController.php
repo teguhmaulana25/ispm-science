07c9dad9275991504772617e2a35cc171e765577
Fauzi Ferdiansyah <fetruzie2@gmail.com>
2019-12-05 01:23:33 +0700

    [add]:: page detail of vacancy

--- app/Http/Controllers/Customer/PagesController.php
+++ app/Http/Controllers/Customer/PagesController.php
@@ -34,6 +34,7 @@ class PagesController extends Controller
       // Get Division
       $data = Division::select(
                   [
+                    'divisions.id',
                     'divisions.name'
                   ]
                 )
@@ -46,37 +47,66 @@ class PagesController extends Controller
           ->with(compact('data','vacancies'));
     }
 
-    private function getAvailableDiv($get_id, $type) {
+    public function detail($id_div, $id_vacancies) {
+      // Get Vacancies
+      if($this->getAvailableDivCond($id_vacancies, $id_div) == 0) {
+         return abort(404);
+      }
+      $data = $this->getAvailableDiv($id_div, 'first', $id_vacancies);
+      return view('customer.pages.detail')
+          ->with(compact('data','id_div'));
+    }
+
+    private function getAvailableDiv($get_id, $type, $get_id_vc = null) {
       $get_vacancy = JobVacancy::select(
                         [
                           'job_vacancies.id',
                           'job_vacancies.title',
+                          'job_vacancies.description',
+                          'job_vacancies.start_date',
                           'job_vacancies.end_date'
                         ]
                       )
                       ->where('job_vacancies.division_id', $get_id)
                       ->where('job_vacancies.display', 1)
                       ->where('job_vacancies.start_date', '<=', Carbon::now())
-                      ->where('job_vacancies.end_date', '>=', Carbon::now())
-                      ->get();
+                      ->where('job_vacancies.end_date', '>=', Carbon::now());
+      if($type == 'first') {
 
-      $data_count = count($get_vacancy);
-      $data_arr = [];
-      if($data_count > 0) {
-        foreach ($get_vacancy as $key => $v) {
-            $data_arr[$key] = [
-                            'id' => $v->id,
-                            'title' => $v->title,
-                            'end_date' => $v->end_date
-                          ];
-            if($this->getAvailableDivCond($v->id, $get_id) == 0) {
-              unset($data_arr[$key]);
-            }
+        $get_vacancy = $get_vacancy->where('job_vacancies.id', $get_id_vc)->first();
+        if(!empty($get_vacancy)) {
+          $data_arr = [
+                        'id' => $get_vacancy->id,
+                        'title' => $get_vacancy->title,
+                        'description' => $get_vacancy->description,
+                        'start_date' => $get_vacancy->start_date,
+                        'end_date' => $get_vacancy->end_date
+                      ];
+        }
+      }else{
+        $get_vacancy = $get_vacancy->get();
+        $data_count = count($get_vacancy);
+
+        $data_arr = [];
+        if($data_count > 0) {
+          foreach ($get_vacancy as $key => $v) {
+              $data_arr[$key] = [
+                              'id' => $v->id,
+                              'title' => $v->title,
+                              'end_date' => $v->end_date
+                            ];
+              if($this->getAvailableDivCond($v->id, $get_id) == 0) {
+                unset($data_arr[$key]);
+              }
+          }
         }
       }
+      
       if($type == 'count'){
         return count($data_arr);
       }else if($type == 'row'){
+        return $data_arr;
+      }else if($type == 'first'){
         return $data_arr;
       }
     	
